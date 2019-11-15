806ec549ad12a5b41a1e25fff05fa36a937f6411
jordi.pons <jordi.pons@typeform.com>
2019-11-15 18:35:17 +0100

    Adds get branches api call

--- src/api/github.js
+++ src/api/github.js
@@ -2,4 +2,6 @@ import request from './request'
 
 const getUser = user => request(`https://api.github.com/users/${user}`)
 
-export { getUser }
+const getBranches = (user, repo) => request(`https://api.github.com/repos/${user}/${repo}/branches`)
+
+export { getUser, getBranches }
