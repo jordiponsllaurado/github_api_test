d65eb93cf810ad8f4a1841f2d368ab63d559dccd
jordi.pons <jordi.pons@typeform.com>
2019-11-15 18:57:12 +0100

    Second get request

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
 import React, { Component } from 'react'
 import './App.css'
-import { getUser } from './api/github'
+import { getUser, getBranches } from './api/github'
 
 const renderLine = (user, key) => <li key={key}><b>{key}</b>: {user[key]}</li>
+const renderBranches = (branch, key) => <li key={key}><b>{key}</b>: {branch.name}</li>
 
 class App extends Component {
   constructor (props) {
     super(props)
-    this.state = { user: {} }
+    this.state = { user: {}, branches: {} }
   }
 
   componentDidMount () {
     getUser('jordiponsllaurado').then(data => {
       this.setState({ user: data.entity })
     })
+    getBranches('jordiponsllaurado', 'github_api_test').then(data => {
+      this.setState({ branches: data.entity })
+    })
   }
 
   render () {
-    const { user } = this.state
+    const { user, branches } = this.state
     return (
       <div className='App'>
-        <ul style={{ listStyle: 'none' }}>
-          {
-            // Loop over the object keys and render each key
-            Object.keys(user).map(key => renderLine(user, key))
-          }
-        </ul>
+        <div className='User Info'>
+          <h1>USER Info</h1>
+          <ul style={{ listStyle: 'none' }}>
+            {
+              Object.keys(user).map(key => renderLine(user, key))
+            }
+          </ul>
+        </div>
+        <div>
+          <h1>Repo Branches</h1>
+          <ul style={{ listStyle: 'none' }}>
+            {
+              Object.keys(branches).map(key => renderBranches(branches[key], key))
+            }
+          </ul>
+        </div>
       </div>
     )
   }
--- src/api/__tests__/github.test.js
+++ src/api/__tests__/github.test.js
@@ -1,15 +1,21 @@
 /* eslint-env jest */
 
-// This commands loads the mocked request.js as defined in __mocks__/request.js
 jest.mock('../request')
 
 const github = require('../github')
 
-// The exact same test using async/await
-describe('#getUser() using async/await', () => {
+describe('#getUser()', () => {
   it('should load user data', async () => {
     const data = await github.getUser('jordiponsllaurado')
     expect(data).toBeDefined()
     expect(data.entity.name).toEqual('Jordi Pons')
+  })
+})
+
+describe('#getBranches()', () => {
+  it('should load branches data', async () => {
+    const data = await github.getBranches('jordiponsllaurado', 'github_api_test')
+    expect(data).toBeDefined()
+    expect(data.entity.name).toEqual('master')
   })
 })
